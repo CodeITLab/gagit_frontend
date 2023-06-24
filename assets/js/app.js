b44d3eb3dc78fceb1dd573f789e1e259d5675d4a
KristijanFištrek <fistrekkristijan@gmail.com>
2023-06-24 11:06:38 +0200

    I 7 navbar component (#8)
    * Added Navbar component module
    
    * Add: Additional styles to the navbar component
    
    * Add: Background bg color changed
    
    * Add: Created a main menu
    
    * Added additional styles for main menu
    
    * Adjusted search box
    
    * Add: Added aside content
    
    * Added basic fonts
    
    * Created aside list items component

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+import createNavbarElement from './components/navbar-component.js';
+import createAsideCategories from './components/aside-categories-component.js'; 
+
+createNavbarElement();
+createAsideCategories();
