2203f22f79123f01959f6ed777c451ed7761ae18
KristijanFištrek <fistrekkristijan@gmail.com>
2023-06-27 20:42:18 +0200

    I 9 post component (#12)
    * post section test
    
    * Add: Navbar icons implemented
    
    * adjusted post section
    
    * login popup
    
    * Add: Added homepage page
    
    * login button and home page
    
    ---------
    
    Co-authored-by: theravenmasque <137091394+theravenmasque@users.noreply.github.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
-import createNavbarElement from './components/navbar-component.js';
+import createNavbarElement from './components/navbar-filter-component/navbar-component.js';
 import createAsideCategories from './components/aside-categories-component.js'; 
+import './components/login-popup.js';
+import './components/back-to-top-button.js';
+
 
 createNavbarElement();
 createAsideCategories();
---
+++ assets/js/components/navbar-filter-component/navbar-component-data.js
@@ -0,0 +1,19 @@
+const iconsPath = '/assets/img/icons/filter-navbar-icons/';
+export const navbarFilterItems = [
+    {
+        "title": "Home",
+        "icon": iconsPath + "home.png"
+    },
+    {
+        "title": "Top",
+        "icon": iconsPath + "arrow.png"
+    },
+    {
+        "title": "Trending",
+        "icon": iconsPath + "arrow-trend-up.png"
+    }, 
+    {
+        "title": "New",
+        "icon": iconsPath + "new.png"
+    }
+];
