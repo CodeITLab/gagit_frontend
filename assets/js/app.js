8d72410d3beff15e21519207940de0bda955a2f3
theravenmasque <137091394+theravenmasque@users.noreply.github.com>
2023-06-27 20:39:04 +0200

    login button and home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import createNavbarElement from './components/navbar-filter-component/navbar-component.js';
 import createAsideCategories from './components/aside-categories-component.js'; 
 import './components/login-popup.js';
+import './components/back-to-top-button.js';
 
 
 createNavbarElement();
--- assets/js/components/login-popup.js
+++ assets/js/components/login-popup.js
@@ -1,18 +1,18 @@
-// Get the modal
-var modal = document.getElementById("login-popup-wrapper");
 
-// Get the button that opens the modal
-var btn = document.getElementById("login-button");
+let modal = document.getElementById("login-popup-wrapper");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+let btn = document.getElementById("login-button");
+
+
+let span = document.getElementsByClassName("close")[0];
+
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function() {
   modal.style.display = "none";
 }
