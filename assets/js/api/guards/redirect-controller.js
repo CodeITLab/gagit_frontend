2819a513be5869be3295aa57149640c6c02d3be9
kristijanfistrek <fistrekkristijan@gmail.com>
2023-06-28 16:52:35 +0200

    Add: Alerted a message for protected routes

--- assets/js/api/guards/redirect-controller.js
+++ assets/js/api/guards/redirect-controller.js
@@ -5,10 +5,11 @@ const protectedRoutes = {
 function isUserAuthenticated() {
     let token = localStorage.getItem('isUserAuthenticated');
     if(!token) {
+        alert("Access denied. Please login before continuing.")
         window.location.href = 'index.html';
     }
 }
 
 function router() {
-    
+
 }
