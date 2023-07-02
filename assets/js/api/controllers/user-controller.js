9f6deb0ff6ca6bac5f46cdc7720d30f35818dd60
kristijanfistrek <fistrekkristijan@gmail.com>
2023-07-02 21:06:44 +0200

    Add: Added user loggin component

--- assets/js/api/controllers/user-controller.js
+++ assets/js/api/controllers/user-controller.js
@@ -1,1 +1,20 @@
 const userDataURL = "https://gaggit.onrender.com/isUserLoggedIn";
+
+async function userLogin() {
+    const response = await fetch(userDataURL);
+    const data = await response.json();
+    authoriseUser(data.authenticated);
+    redirectTheUser();
+}
+
+function authoriseUser(isUserAuthenticated) {
+    if(isUserAuthenticated) {
+        localStorage.setItem("isUserAuthenticated", isUserAuthenticated.toString());    
+    }
+}
+
+function redirectTheUser() {
+    if(localStorage.getItem('isUserAuthenticated')) {
+        window.location.href = 'homepage.html';
+    }
+}
