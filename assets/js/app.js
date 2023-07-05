d6ceea43eee93cd640e69873ffce249846a82217
KristijanFištrek <fistrekkristijan@gmail.com>
2023-07-05 19:35:43 +0200

    Revert "post page ten point 0"
    This reverts commit d5b3b5ff9a7c075c1c2d758e688b7e4cc08147b9.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ import createAsideCategories from './components/aside-categories-component/aside
 import asideFollowList from './components/aside-followers-component/aside-categories-follow-components.js';
 
 import './utilities/back-to-top-button.js';
+import './utilities/comment-button.js';
 
 createNavbarElement();
 createAsideCategories();
---
+++ assets/js/utilities/comment-button.js
@@ -0,0 +1,12 @@
+let div = document.getElementById("comment-posting-display");
+let btn = document.getElementById("post-a-comment-button");
+
+btn.onclick = function() {
+  div.style.display =  "block";  
+}
+
+window.onclick = function(event){
+    if (event.target == div){
+        div.style.display = "none";
+    }
+}
